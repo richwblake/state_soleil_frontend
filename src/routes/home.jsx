3f002f1a1a3d4b6f9e3d18c7d41229c4173d9d5a
Wills Blake <richwblake@gmail.com>
2023-11-21 13:30:48 -0500

    Adds main image to home page, with overlaying text and base styling

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,11 @@
+import './home.css';
+
 export default function Home() {
     return (
         <div id='home-page'>
-            <h1>home page</h1>
+            <div id='home-main-img'>
+                <h1>Using old world style to reinvent florals</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore </p> </div>
         </div>
     );
 }
