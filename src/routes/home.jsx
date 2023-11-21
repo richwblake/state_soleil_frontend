3484312de1bfbfc7abf0b3d52fef9d9cd45345c9
Wills Blake <richwblake@gmail.com>
2023-11-21 14:46:00 -0500

    Adds portfolio link with picture, refactors css to keep consistency

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,6 +1,7 @@
 import './home.css';
 import { Link } from 'react-router-dom';
 import AboutPhoto from '../assets/main-about-photo.jpg';
+import PortfolioPhoto from '../assets/main-portfolio.jpg';
 import ContactBtn from '../components/contactbtn';
 import RightArrow from '../components/rightarrow';
 
@@ -17,11 +18,19 @@ export default function Home() {
                 <p>ABOUT THE COMPANY HERE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
                 <ContactBtn btnText={"get in touch"} />
             </div>
-            <div id='home-about'>
-                <Link to='about'>
-                    <img id='home-about-img' src={AboutPhoto} />
+            <div className='home-link-container'>
+                <Link className='home-link' to='about'>
+                    <img className='home-link-imgs' src={AboutPhoto} />
                     <h1>About</h1>
                     <p>Learn more about our ambitions</p>
+                    <RightArrow />
+                </Link>
+            </div>
+            <div className='home-link-container'>
+                <Link className='home-link' to='portfolio'>
+                    <img className='home-link-imgs' src={PortfolioPhoto} />
+                    <h1>Portfolio</h1>
+                    <p>See our work</p>
                     <RightArrow />
                 </Link>
             </div>
