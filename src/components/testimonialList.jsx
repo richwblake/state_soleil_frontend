86ce6355cf47dd8998d33728cb31a748d705b5e8
Wills Blake <richwblake@gmail.com>
2023-11-21 16:01:05 -0500

    Changes from showing all testimonials to a single based on a currentTest state variable

--- src/components/testimonialList.jsx
+++ src/components/testimonialList.jsx
@@ -1,23 +1,32 @@
 import Testimonial from './testimonial';
+import { useState } from 'react';
 
 export default function TestimonialList() {
+
+    const [currentTestId, setCurrentTestId] = useState(1);
+
     const testimonials = [
         {
+            id: 1,
             content: "Testimonial 1",
             author: "Person 1",
             imgUrl: "/src/assets/testimonial1.jpg",
         },
         {
+            id: 2,
             content: "Testimonial 2",
             author: "Person 2",
             imgUrl: "/src/assets/testimonial2.jpg",
         },
         {
+            id: 3,
             content: "Testimonial 3",
             author: "Person 3",
             imgUrl: "/src/assets/testimonial3.jpg",
         },
     ];
 
-    return testimonials.map(t => <Testimonial key={t.content} content={t.content} author={t.author} imgUrl={t.imgUrl} />);
+    const currentTest = testimonials.find(t => t.id === currentTestId);
+
+    return <Testimonial content={currentTest.content} author={currentTest.author} imgUrl={currentTest.imgUrl} />;
 }
