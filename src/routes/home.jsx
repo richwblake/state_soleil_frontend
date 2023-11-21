c7848f2ffc9de8ee05155bb5cc5fbfc742c04634
Wills Blake <richwblake@gmail.com>
2023-11-21 14:31:18 -0500

    Adds about link and picture. Adds right arrow icon for link component

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,5 +1,8 @@
 import './home.css';
+import { Link } from 'react-router-dom';
+import AboutPhoto from '../assets/main-about-photo.jpg';
 import ContactBtn from '../components/contactbtn';
+import RightArrow from '../components/rightarrow';
 
 export default function Home() {
     return (
@@ -13,6 +16,14 @@ export default function Home() {
                 <span>Brief company explanation or value of operation</span>
                 <p>ABOUT THE COMPANY HERE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
                 <ContactBtn btnText={"get in touch"} />
+            </div>
+            <div id='home-about'>
+                <Link to='about'>
+                    <img id='home-about-img' src={AboutPhoto} />
+                    <h1>About</h1>
+                    <p>Learn more about our ambitions</p>
+                    <RightArrow />
+                </Link>
             </div>
         </div>
     );
