e95bccc1e2f9261a53c46d13936b6e8b6d37ac30
Wills Blake <richwblake@gmail.com>
2024-03-01 17:21:01 -0500

    adds new photos to portfolio

--- src/routes/portfolio.jsx
+++ src/routes/portfolio.jsx
@@ -8,20 +8,37 @@ import Port6 from '../assets/port6.jpg';
 import Port7 from '../assets/port7.jpg';
 import Port8 from '../assets/port8.jpg';
 import Port9 from '../assets/port9.jpg';
+import Port10 from '../assets/port10.jpg';
+import Port11 from '../assets/port11.jpg';
+import Port12 from '../assets/port12.jpg';
+import Port13 from '../assets/port13.jpg';
+import Port14 from '../assets/port14.jpg';
+import Port15 from '../assets/port15.jpg';
+import Port16 from '../assets/port16.jpg';
+import Port17 from '../assets/port17.jpg';
+import Port18 from '../assets/port18.jpg';
 import HeaderImage from '../components/headerImage';
 
 export default function Portfolio() {
     return (
         <div id='portfolio-page'>
             <img src={Port1} />
-            <img src={Port2} />
             <img src={Port3} />
             <img src={Port5} />
             <img src={Port6} />
             <img src={Port7} />
+            <img src={Port2} />
             <img src={Port8} />
             <img src={Port9} />
-            <img src={Port1} />
+            <img src={Port10} />
+            <img src={Port13} />
+            <img src={Port11} />
+            <img src={Port12} />
+            <img src={Port14} />
+            <img src={Port15} />
+            <img src={Port16} />
+            <img src={Port17} />
+            <img src={Port18} />
         </div>
     );
 }
