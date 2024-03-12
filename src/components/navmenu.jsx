688545507af288300cff1af8dd79245a0f9ab06e
Wills Blake <richwblake@gmail.com>
2024-03-12 16:09:05 -0400

    Corrects layout bugs

--- src/components/navmenu.jsx
+++ src/components/navmenu.jsx
@@ -8,7 +8,6 @@ export default function NavMenu({ setIsOpen }) {
             <Link onClick={() => setIsOpen(prev => !prev)} to='about'>About</Link>
             <Link onClick={() => setIsOpen(prev => !prev)} to='contact'>Inquire</Link>
             <Link onClick={() => setIsOpen(prev => !prev)} to='portfolio'>Portfolio</Link>
-            <Link onClick={() => setIsOpen(prev => !prev)} to='offerings'>Offerings</Link>
         </div>
     );
 }
