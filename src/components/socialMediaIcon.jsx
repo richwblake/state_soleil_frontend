e79bc33fd68ae0699e9942cc3754f95207d65603
Wills Blake <richwblake@gmail.com>
2025-09-22 21:53:45 -0400

    Removes individual social media components abstracting into single component

---
+++ src/components/socialMediaIcon.jsx
@@ -0,0 +1,16 @@
+export default function SocialMediaIcon({ svgMap, linkTo }) {
+  return (
+    <div className="footer-sm-btn">
+      <a target="_blank" href={linkTo}>
+        <svg
+          className="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 496 512"
+        >
+          <path d={svgMap} />
+        </svg>
+      </a>
+    </div>
+  );
+}
