bee2cc47634b062dd07713971dac1ca7aaad91a6
Wills Blake <richwblake@gmail.com>
2025-09-24 18:32:28 -0400

    Image compressor in v1 state but with errors

--- utils/imageProcessor.js
+++ utils/imageProcessor.js
@@ -3,10 +3,19 @@ import path from "path";
 import sharp from "sharp";
 
 const inputDir = "./src/assets";
-const outputDirs = {
-  landscape: "./src/assets/landscapes",
-  portrait: "./src/assets/portraits",
-  universal: "./src/assets/universal",
+const config = {
+  landscape: {
+    path: "./src/assets/landscapes",
+    width: 1920,
+  },
+  portrait: {
+    path: "./src/assets/portraits",
+    width: 1200,
+  },
+  universal: {
+    path: "./src/assets/universal",
+    width: 1200,
+  },
 };
 
 fs.readdirSync(inputDir).forEach(async (image) => {
@@ -16,15 +25,32 @@ fs.readdirSync(inputDir).forEach(async (image) => {
   if (image == "landscapes" || image == "portraits" || image == "universal")
     return;
 
-  let outputDir = "";
+  let output = {};
 
-  if (image.includes("mb")) {
-    outputDir = outputDirs["portrait"];
-  } else if (image.includes("dt")) {
-    outputDir = outputDirs["landscape"];
+  if (image.includes("-mb")) {
+    output.path = path.join(
+      config.portrait.path,
+      image.replace(/\.\w+$/, ".webp")
+    );
+    output.width = config.portrait.width;
+  } else if (image.includes("-dt")) {
+    output.path = path.join(
+      config.landscape.path,
+      image.replace(/\.\w+$/, ".webp")
+    );
+    output.width = config.landscape.width;
   } else {
-    outputDir = outputDirs["universal"];
+    output.path = path.join(
+      config.universal.path,
+      image.replace(/\.\w+$/, ".webp")
+    );
+    output.width = config.universal.width;
   }
 
-  console.log(`${image} -> ${outputDir}`);
+  await sharp(inputPath)
+    .resize({ width: output.width })
+    .webp({ quality: 80 })
+    .toFile(output.path);
+
+  console.log(`Resized ${image} -> ${output.path}`);
 });
