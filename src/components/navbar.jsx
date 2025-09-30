a88fcef889f0e07835b246dfccaeac278fa9f867
Wills Blake <richwblake@gmail.com>
2025-09-30 16:55:14 -0400

    Adds link to main title in navbar to go home

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import Hamburger from "hamburger-react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 export default function Navbar({ isOpen, setIsOpen }) {
   const location = useLocation();
@@ -11,9 +11,12 @@ export default function Navbar({ isOpen, setIsOpen }) {
         className={`${isOpen ? "invisible" : "visible"} flex flex-col`}
         id="nav-text"
       >
-        <span id="logo" className="text-2xl">
-          STATE SOLEIL
-        </span>
+        <Link to={"/"}>
+          {" "}
+          <span id="logo" className="text-2xl">
+            STATE SOLEIL
+          </span>
+        </Link>
         <p id="path-title" className="birthstone-regular text-3xl">
           {pathname.substring(1)}
         </p>
