fcffa3ceb547b55985833a15d35497e6cf8be53c
Wills Blake <richwblake@gmail.com>
2025-10-02 18:23:11 -0400

    Working on inquiry form tailwind styling

--- src/components/inquiryForm.jsx
+++ src/components/inquiryForm.jsx
@@ -24,77 +24,108 @@ export default function ContactForm() {
 
   return (
     <Form method="post" id="inquiry-form">
-      <h3>Your Name</h3>
-      <div className="input-container">
-        <input
-          id="first-name"
-          autoComplete="off"
-          type="text"
-          name="firstName"
-        />
-        <label htmlFor="first-name">First Name</label>
+      <div className="grid grid-cols-2 w-full md:w-3/4 mx-auto border">
+        <h3 className="col-span-2 mb-4">Your Name</h3>
+        <div className="input-container md:max-w-3/4">
+          <input
+            id="first-name"
+            autoComplete="off"
+            type="text"
+            name="firstName"
+            className="border rounded w-full "
+          />
+          <label className="block text-xs m-1" htmlFor="first-name">
+            First Name
+          </label>
+        </div>
+        <div className="justify-self-end md:max-w-3/4">
+          <input
+            id="last-name"
+            autoComplete="off"
+            type="text"
+            name="lastName"
+            className="border rounded w-full "
+          />
+          <label className="block text-xs m-1" htmlFor="last-name">
+            Last Name
+          </label>
+        </div>
+        <h3 className="col-span-2 my-4">Contact Information</h3>
+        <div id="contact-info-container">
+          <input
+            id="email"
+            autoComplete="email"
+            type="text"
+            name="email"
+            className="border rounded w-full md:max-w-3/4"
+            required
+          />
+          <label className="block text-xs m-1" htmlFor="email">
+            E-mail Address
+          </label>
+        </div>
+        <div className="justify-self-end md:max-w-3/4">
+          <input
+            id="phone"
+            type="text"
+            autoComplete="tel"
+            name="phone"
+            className="border rounded w-full"
+            required
+          />
+          <label className="block text-xs m-1" htmlFor="phone">
+            Phone Number
+          </label>
+        </div>
+        <h3 className="col-span-2 my-4">Event Details</h3>
+        <div className="input-container">
+          <select
+            id="event-type"
+            className="event-type"
+            name="eventType"
+            defaultValue="placeholder"
+          >
+            <option value="placeholder" hidden>
+              Choose an event
+            </option>
+            <option value="wedding">Wedding</option>
+            <option value="corporate">Corporate</option>
+            <option value="editorial">Editorial</option>
+            <option value="party">Party</option>
+            <option value="other">Other</option>
+          </select>
+          <label className="block text-xs m-1" htmlFor="event-type">
+            Event Type
+          </label>
+        </div>
+        <div className="input-container">
+          <input
+            id="event-date"
+            className="event-details"
+            defaultValue={new Date().toISOString().substring(0, 10)}
+            type="date"
+            name="date"
+          />
+          <label className="block text-xs m-1" htmlFor="event-date">
+            Date of Event
+          </label>
+        </div>
+        <div className="col-span-2">
+          <label className="block text-xs m-1" htmlFor="notes">
+            Additional Notes
+          </label>
+          <textarea
+            rows={5}
+            id="notes"
+            type="textbox"
+            name="notes"
+            className="border rounded w-full"
+          />
+        </div>
+        <button type="submit" className="cormorant-garamond-regular col-span-2">
+          SUBMIT INQUIRY
+        </button>
       </div>
-      <div className="input-container">
-        <input id="last-name" autoComplete="off" type="text" name="lastName" />
-        <label htmlFor="last-name">Last Name</label>
-      </div>
-      <h3>Contact Information</h3>
-      <div id="contact-info-container">
-        <input
-          id="email"
-          autoComplete="email"
-          type="text"
-          name="email"
-          required
-        />
-        <label htmlFor="email">E-mail Address</label>
-      </div>
-      <div className="input-container">
-        <input
-          id="phone"
-          type="text"
-          autoComplete="tel"
-          name="phone"
-          required
-        />
-        <label htmlFor="phone">Phone Number</label>
-      </div>
-      <h3>Event Details</h3>
-      <div className="input-container">
-        <select
-          id="event-type"
-          className="event-type"
-          name="eventType"
-          defaultValue="placeholder"
-        >
-          <option value="placeholder" hidden>
-            Choose an event
-          </option>
-          <option value="wedding">Wedding</option>
-          <option value="corporate">Corporate</option>
-          <option value="editorial">Editorial</option>
-          <option value="party">Party</option>
-          <option value="other">Other</option>
-        </select>
-        <label htmlFor="event-type">Event Type</label>
-      </div>
-      <div className="input-container">
-        <input
-          id="event-date"
-          className="event-details"
-          defaultValue={new Date().toISOString().substring(0, 10)}
-          type="date"
-          name="date"
-        />
-        <label htmlFor="event-date">Date of Event</label>
-      </div>
-      <div className="input-container">
-        <label htmlFor="notes">Additional Notes</label>
-        <textarea rows={4} cols={40} id="notes" type="textbox" name="notes" />
-      </div>
-      <button type="submit" className="cormorant-garamond-regular">
-        SUBMIT INQUIRY
-      </button>
     </Form>
   );
 }
